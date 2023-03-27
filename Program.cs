f86650eaf225b27a7259819aa4e3d8ccbab97fd3
Daria Yakkola <yakkolabest@gmail.com>
2023-03-27 21:19:22 +0300

    Отработа возможная ошибка ввода сообщения через пробелы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿Console.Write("Введите сообщение через (;) без использования пробелов: ");
 string message = Console.ReadLine();
+/// <summary>
+/// Метод, который печатает элементы массива состоящие не более чем из 3 символов
+/// </summary>
+/// <param name="array">Массив элементов</param>
+void PrintArray3Chars (char[] array)
+{
+int size = array.Length;
+for (int j = 0; j < size; j++)
+{
+  if (array[j] == ' ')
+  {
+    Console.Write("Массив введен не корректно, используются пробелы!");
+    return;
+  }
+}
+}
