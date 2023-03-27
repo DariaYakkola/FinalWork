b5bb718f7438a27ca31e649475fc0b4e11f7b8d3
Daria Yakkola <yakkolabest@gmail.com>
2023-03-27 21:20:42 +0300

    Сделан метод, который выводит элементы

--- Program.cs
+++ Program.cs
@@ -15,4 +15,23 @@ for (int j = 0; j < size; j++)
     return;
   }
 }
+int count = -1;
+for (int i = 0; i < size; i++)
+{
+  count++;
+  if (array[i] == ';' && count <= 3)
+  {
+    while (array[i - count] != ';')
+    {
+      Console.Write(array[i - count]);
+      count--;
+    }
+    count = -1;
+    Console.Write(";");
+  }
+  else if (array[i] == ';' && count > 3)
+  {
+    count = -1;
+  }
+}
 }
